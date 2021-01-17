31e00fe0ea904dbae105cf8e0b8c6e5ddc352a01
bing <qlstks@gmail.com>
2021-01-18 03:24:48 +0900

    update ui(divider)

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import { getWithExpiry, setWithExpiry } from './utils'
 import './App.css'
 export default function App() {
   const professors = [
-    '김탁희',
-    '오창희',
-    '유태영',
-    '변승환',
-    '김준호',
-    '김재석',
-    '송빈산',
-    '김선재',
-    '이철민',
-    '이민교',
-    '유창오',
-    '김도영',
-    '김구현',
+    { name: '김탁희', region: '서울', classNo: '1반' },
+    { name: '오창희', region: '서울', classNo: '1반' },
+    { name: '유태영', region: '서울', classNo: '2반' },
+    { name: '변승환', region: '서울', classNo: '3반' },
+    { name: '김준호', region: '서울', classNo: '4반' },
+    { name: '김재석', region: '서울', classNo: '5반' },
+    { name: '송빈산', region: '대전', classNo: '1반' },
+    { name: '김선재', region: '대전', classNo: '2반' },
+    { name: '이철민', region: '대전', classNo: '3반' },
+    { name: '이민교', region: '광주', classNo: '1반' },
+    { name: '유창오', region: '광주', classNo: '2반' },
+    { name: '김도영', region: '구미', classNo: '1반' },
+    { name: '김구현', region: '구미', classNo: '2반' },
   ]
 
   const [selected, setSelected] = useState('')
@@ -66,18 +66,27 @@ export default function App() {
       <h3 className="text-center my-4">데일리 건강 설문</h3>
       <div className="center">
         <div className="inputs">
-          {professors.map((professor) => (
-            <button
-              className={
-                selected === professor ? 'btn btn-success' : 'btn btn-secondary'
-              }
-              onClick={() => {
-                setSelected(professor)
-              }}
-            >
-              {professor}
-            </button>
-          ))}
+          {professors.map((professor, index) => {
+            const { name, region } = professor
+            const prevRegion = index === 0 ? '' : professors[index - 1].region
+            return (
+              <>
+                {region === prevRegion ? null : (
+                  <hr data-content={region} class="hr-text"></hr>
+                )}
+                <button
+                  className={
+                    selected === name ? 'btn btn-success' : 'btn btn-secondary'
+                  }
+                  onClick={() => {
+                    setSelected(name)
+                  }}
+                >
+                  {name}
+                </button>
+              </>
+            )
+          })}
         </div>
 
         <button className="btn btn-primary" onClick={onSubmit}>
